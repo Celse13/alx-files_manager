4d2a21d57521ab7481d0a5c9d258afaacafaeda7
celse13 <rugiracelse13@gmail.com>
2024-05-13 14:18:00 +0200

    Creating Redis Client

--- utils/redis.js
+++ utils/redis.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
     this.client.on('error', (error) => {
       console.error(error);
@@ -11,21 +11,21 @@ class RedisClient {
     this.getAsync = promisify(this.client.get).bind(this.client);
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
-  async get (key) {
+  async get(key) {
     const value = await this.getAsync(key);
     return value;
   }
 
-  async set (key, value, duration) {
+  async set(key, value, duration) {
     this.client.set(key, value);
     this.client.expire(key, duration);
   }
 
-  async del (key) {
+  async del(key) {
     this.client.del(key);
   }
 }
